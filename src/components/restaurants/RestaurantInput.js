400e610dd530b805a422e1bdfd49c2065656a4ac
github username <github email address>
2020-06-26 15:47:30 -0700

    Done.

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,13 +1,37 @@
 import React, { Component } from 'react';
 
 class RestaurantInput extends Component {
-  render() {
-    return (
-      <div>
-        Restaurant Input
-      </div>
-    );
-  }
+    constructor(props){
+        super(props)
+        this.state = {
+            text: ""
+        }
+    }
+
+    handleChange = event => {
+        this.setState({
+            text: event.target.value
+        })
+    }
+
+    handleSubmit = event => {
+        event.preventDefault()
+        this.props.addRestaurant(this.state.text)
+        this.setState({
+            text: ""
+        })
+    }
+  
+    render() {
+        return (
+        <div>
+            <form onSubmit={this.handleSubmit}>
+                <input type="text" onChange={this.handleChange} value={this.state.text} />
+                <input type="submit" />
+            </form>
+        </div>
+        );
+    }
 };
 
 export default RestaurantInput;
--- src/components/reviews/ReviewInput.js
+++ src/components/reviews/ReviewInput.js
@@ -2,13 +2,39 @@ import React, { Component } from 'react';
 import Reviews from './Reviews';
 
 class ReviewInput extends Component {
-  render() {
-    return (
-      <div>
-        Review Input
-      </div>
-    );
-  }
+    constructor(props){
+        super(props)
+
+        this.state = {
+            text: ""
+        }
+    }
+  
+    handleChange = event => {
+        this.setState({
+            text: event.target.value
+        })
+    }
+
+    handleSubmit = event => {
+        event.preventDefault()
+        this.props.addReview({text: this.state.text, restaurantId: this.props.restaurantId})
+        this.setState({
+            text: ""
+        })
+    }
+
+    render() {
+        return (
+        <div>
+            <form onSubmit={this.handleSubmit}>
+                <label>Add Review</label>
+                <input type="text" onChange={this.handleChange} value={this.state.text}/>
+                <input type="submit" />
+            </form>
+        </div>
+        );
+    }
 };
 
 export default ReviewInput;
