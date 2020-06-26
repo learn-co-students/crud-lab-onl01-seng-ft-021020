7b42be0cb4a34dcce290d9ef98a2e5508de0a219
biagioo <mendolia2328@gmail.com>
2020-06-26 13:47:40 -0400

    Done.

--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
-
+import Restaurant from './Restaurant'
 class Restaurants extends Component {
   render() {
     return(
       <ul>
         Restaurants Component
+        {/* iterate Restaurants and call <Restaurant /> */}
+        {this.props.restaurants.map(restaurant => <Restaurant 
+        deleteRestaurant={this.props.deleteRestaurant}
+        key={restaurant.id}
+        restaurant={restaurant}/>
+        )}
       </ul>
     );
   }
--- src/components/reviews/Review.js
+++ src/components/reviews/Review.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
 
 class Review extends Component {
+  handleOnClick = id =>{
+    this.props.deleteReview(id)
+  }
 
   render() {
-    const { review } = this.props
-
+    const { review,  } = this.props
     return (
       <div>
         <li>
           {review.text}
         </li>
-        <button> X </button>
+        <button
+        onClick={()=> this.handleOnClick(review.id)}> X </button>
       </div>
     );
   }
